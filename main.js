123008292baf25556657a6d23aec2fa373c4a645
InvalidSB <invalidsb45@gmail.com>
2020-09-26 08:54:52 +0545

    Latest commit

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
     $('.slider').ripples({
-<<<<<<< HEAD
         dropRadius: 12,
         perturbance: 0.001,
-=======
-        dropRadius: 10,
-        perturbance: 0.01,
->>>>>>> 7cc036110d038a083c1cd9ca1aea501da6945cd9
-    
+        
       });
 })
